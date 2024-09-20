2cee5e3cac4a15aa5dc6ae8e643e6833858eac4a
parkyunsir <qdt1030@naver.com>
2024-09-20 17:51:14 +0900

    User 엔티티

--- backend/src/main/java/com/example/backend/model/UsersEntity.java
+++ backend/src/main/java/com/example/backend/model/UsersEntity.java
@@ -18,7 +18,7 @@ public class UsersEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    private Long kakaoId; // 카카오 고유 ID
+    private Long kakaoId; // 카카오 고유 ID
     private String nickname;
     private String thumbnailImageUrl;
     private String profileImageUrl;
