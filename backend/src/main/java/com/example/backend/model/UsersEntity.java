a3b823fca7f461c159abdc51346185ceb5a7df5f
heeyun817 <vmffpfm79@naver.com>
2024-09-21 13:06:19 +0900

    feat : User엔티티에 카테고리 추가

--- backend/src/main/java/com/example/backend/model/UsersEntity.java
+++ backend/src/main/java/com/example/backend/model/UsersEntity.java
@@ -1,6 +1,8 @@
 package com.example.backend.model;
 
+import com.example.backend.model.enums.Category;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,4 +24,9 @@ public class UsersEntity {
     private String nickname;
     private String thumbnailImageUrl;
     private String profileImageUrl;
+
+    @ElementCollection(targetClass = Category.class)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
+    private List<Category> categories;
 }
