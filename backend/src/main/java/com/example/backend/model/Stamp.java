0a868dd37ebe7b6f9755bbbb017952b9d043783e
heeyun817 <vmffpfm79@naver.com>
2024-09-21 14:13:40 +0900

    feat : Add Stamp Entity
    프리퀀시 기능을 위한 Stamp 엔티티 추가

---
+++ backend/src/main/java/com/example/backend/model/Stamp.java
@@ -0,0 +1,36 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Stamp {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private UsersEntity user;
+
+  @ManyToOne
+  @JoinColumn(name = "landmark_id", nullable = false)
+  private Landmark landmark;
+
+  @Column(nullable = false)
+  private String earnedDate; // 적립한 날짜
+}
