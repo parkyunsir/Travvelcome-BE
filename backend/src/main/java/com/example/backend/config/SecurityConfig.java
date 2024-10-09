1ab69664ebe774b8f8afaaf846f10617f279ded3
parkyunsir <qdt1030@naver.com>
2024-10-09 18:21:08 +0900

    docs : CustomOAuth2UserService 주석처리

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.example.backend.config;
 
-import com.example.backend.config.oauth.CustomOAuth2UserService;
+//import com.example.backend.config.oauth.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -18,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf().disable() // test 과정에서만 비활성화
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/", "/login", "/callback/**").permitAll()  // requestMatchers로 변경
               .anyRequest().permitAll() // 일단 이렇게..
@@ -26,19 +21,16 @@ public class SecurityConfig {
             )
             .oauth2Login(oauth2 -> oauth2
                 .defaultSuccessUrl("/main", true)  // 로그인 성공 후 리다이렉트할 URL
-                .userInfoEndpoint(userInfo -> userInfo
-                        .userService(kakaoOAuth2UserService())  // 사용자 정보를 처리하는 서비스 설정
-                )
             );
         return http.build();
     }
 
     // 사용자 정보를 받아오는 Kakao OAuth2UserService 설정
-    @Bean
-    public OAuth2UserService<OAuth2UserRequest, OAuth2User> kakaoOAuth2UserService() {
-        return userRequest -> {
-            OAuth2User oAuth2User = new CustomOAuth2UserService().loadUser(userRequest);
-            return oAuth2User;
-        };
-    }
+//    @Bean
+//    public OAuth2UserService<OAuth2UserRequest, OAuth2User> kakaoOAuth2UserService() {
+//        return userRequest -> {
+//            OAuth2User oAuth2User = new CustomOAuth2UserService().loadUser(userRequest);
+//            return oAuth2User;
+//        };
+//    }
 }
